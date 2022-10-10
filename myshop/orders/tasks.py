2a008d32b06913f0ca9f7dff00354ad3018b29e5
Zhalgas <Nurakhmetov997@yandex.ru>
2022-10-11 00:04:38 +0300

    celery task, refactor views to send email after making order

---
+++ myshop/orders/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from .models import Order
+
+
+@shared_task
+def order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f"Заказ номер {order.id}"
+    message = (
+        f"Уважаемый {order.first_name},\n\n"
+        f"Вы успешно оформили заказ "
+        f"Номер Вашего заказа - {order.id}."
+    )
+    mail_sent = send_mail(subject, message, "admin@pleakley.com", [order.email])
+    return mail_sent
