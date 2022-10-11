46e13f4d4ab4c3878adc497d7b3ddd595c6bf7a4
Zhalgas <Nurakhmetov997@yandex.ru>
2022-10-11 22:13:04 +0300

    Custom command in admin panel to generate CSV file. Custom admin template for Detail View of Order

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,4 +1,9 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
@@ -13,6 +18,37 @@ def order_payment(obj: Order):
 
 
 order_payment.short_description = "оплата на Stripe"
+
+
+def order_detail(obj):
+    url = reverse("orders:admin_order_detail", args=[obj.pk])
+    return mark_safe(f'<a href="{url}">Обзор</a>')
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f"attachment; filename={opts.verbose_name}.csv"
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = "Экспорт в CSV"
 
 
 class OrderItemInline(admin.TabularInline):
@@ -34,7 +70,9 @@ class OrderAdmin(admin.ModelAdmin):
         order_payment,
         "created",
         "updated",
+        order_detail,
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
     readonly_fields = ["stripe_id"]
